40383e47f847610ba4b08ea24286d4ee2e7f0fbc
Alex-KC Li <alex.kcli99@gmail.com>
2022-04-24 17:24:18 -0500

    added residence hall data, changed pokemon-app.js

--- js/pokemon-app.js
+++ js/pokemon-app.js
@@ -1,4 +1,5 @@
-const MAP_SIZE = 500
+// const MAP_SIZE = 500
+const MAP_SIZE = 700
 const NU_CENTER = ol.proj.fromLonLat([-87.6753, 42.056])
 
 // downtown center, uncomment to use downtown instead, or make your own
@@ -27,11 +28,11 @@ let map = new InteractiveMap({
 	mapCenter: NU_CENTER,
 
 	// Ranges
-	ranges: [500, 200, 90, 1], // must be in reverse order
+	ranges: [500, 200, 90, 15], // must be in reverse order
 
 	initializeMap() {
 		// A good place to load landmarks
-		this.loadLandmarks("landmarks-shop-evanston", (landmark) => {
+		this.loadLandmarks("landmarks-dorms-nu", (landmark) => {
 			// Keep this landmark?
 
 			// Keep all landmarks in the set
@@ -41,18 +42,18 @@ let map = new InteractiveMap({
 			// return landmark.properties.amenity || landmark.properties.store
 		})
 
-		// Create random landmarks
-		// You can also use this to create trails or clusters for the user to find
-		for (var i = 0; i < 10; i++) {
+		// // Create random landmarks
+		// // You can also use this to create trails or clusters for the user to find
+		// for (var i = 0; i < 10; i++) {
 
-			// make a polar offset (radius, theta) 
-			// from the map's center (units are *approximately* meters)
-			let position = clonePolarOffset(NU_CENTER, 400*Math.random() + 300, 20*Math.random())
-			this.createLandmark({
-				pos: position,
-				name: words.getRandomWord(),
-			})
-		}
+		// 	// make a polar offset (radius, theta) 
+		// 	// from the map's center (units are *approximately* meters)
+		// 	let position = clonePolarOffset(NU_CENTER, 400*Math.random() + 300, 20*Math.random())
+		// 	this.createLandmark({
+		// 		pos: position,
+		// 		name: words.getRandomWord(),
+		// 	})
+		// }
 	},
 
 	update() {
@@ -66,6 +67,7 @@ let map = new InteractiveMap({
 		if (landmark.openMapData) {
 			console.log(landmark.openMapData)
 			landmark.name = landmark.openMapData.name
+			landmark.points = parseInt(landmark.openMapData['addr:housenumber'], 10) % 10
 		}
 		
 		// *You* decide how to create a marker
@@ -74,7 +76,9 @@ let map = new InteractiveMap({
 		landmark.color = [Math.random(), 1, .5]
 
 		// Give it a random number of points
-		landmark.points = Math.floor(Math.random()*10 + 1)
+		// landmark.points = Math.floor(Math.random()*10 + 1)
+		// landmark.points = parseInt(landmark.openMapData['addr:housenumber'], 10) % 10
+		
 		return landmark
 	}, 
 
@@ -83,18 +87,15 @@ let map = new InteractiveMap({
 		// -1 is not in any range
 
 		console.log("enter", landmark.name, newLevel)
-		if (newLevel == 2) {
-
+		if (newLevel == 3) {
 			// Add points to my gamestate
-			gameState.points += landmark.points
-
-			
 
 			// Have we captured this?
 			if (!gameState.captured.includes(landmark.name)) {
+				gameState.points += landmark.points
 				gameState.captured.push(landmark.name)
 				// Add a message
-				gameState.messages.push(`You captured ${landmark.name} for ${landmark.points} points`)
+				gameState.messages.push(`You entered ${landmark.name} and earned ${landmark.points} points`)
 			}
 
 		}
@@ -150,12 +151,13 @@ window.onload = (event) => {
 			<div id="main-columns">
 
 				<div class="main-column" style="flex:1;overflow:scroll;max-height:200px">
-					(TODO, add your own gamestate)
-					{{gameState}}
-					
+					Enter 15 meter radius of residence halls to earn points <br>
+					Your total points: {{gameState.points}} <br>
+					<!-- You entered: {{gameState.captured}} <br> -->
+					<div v-for="msg in gameState.messages"> {{msg}} </div>
 				</div>
 
-				<div class="main-column" style="overflow:hidden;width:${MAP_SIZE}px;height:${MAP_SIZE}px">
+				<div class="main-column" style="overflow:hidden;width:${MAP_SIZE-100}px;height:${MAP_SIZE}px">
 					<location-widget :map="map" />
 				
 				</div>
